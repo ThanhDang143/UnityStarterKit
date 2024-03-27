9fa24e385c72e347cf1f164d7259310f254c73be
anh-pham <anhpt.csit@gmail.com>
2024-03-27 17:27:01 +0700

    Add comments

--- SSv5/Assets/ThirdParties/SS/Screen/Scripts/ScreenManager.cs
+++ SSv5/Assets/ThirdParties/SS/Screen/Scripts/ScreenManager.cs
@@ -30,10 +30,10 @@ public class ScreenManager : MonoBehaviour
     private GameObject m_Loading;
     #endregion
 
-    #region Static
+    #region Private Static
     private static ScreenManager m_Instance;
 
-    public static ScreenManager instance
+    private static ScreenManager instance
     {
         get
         {
@@ -45,53 +45,108 @@ public class ScreenManager : MonoBehaviour
             return m_Instance;
         }
     }
+    #endregion
 
+    #region Public Static
+    /// <summary>
+    /// Get the scene loading operation. Can get some values like % progress.
+    /// </summary>
     public static AsyncOperation asyncOperation
     {
         get;
         protected set;
     }
 
+    /// <summary>
+    /// Set some basic parameters of ScreenManager.
+    /// </summary>
+    /// <param name="screenShieldColor">The color of screen shield</param>
+    /// <param name="screenPath">The path (in Resources folder) of screen's prefabs</param>
+    /// <param name="screenAnimationPath">The path (in Resources folder) of screen's animation clips</param>
+    /// <param name="sceneLoadingName">The name of the scene loading screen which is put in 'screenPath'. Set it to empty if you do not want to show the loading screen while loading a scene</param>
+    /// <param name="loadingName">The name of the loading screen which is put in 'screenPath'. This screen can show/hide on the top of all screens at any time using Loading(bool). Set it to empty if you don't need</param>
     public static void Set(Color screenShieldColor, string screenPath = "Screens", string screenAnimationPath = "Animations", string sceneLoadingName = "", string loadingName = "")
     {
         instance.Setup(screenShieldColor, screenPath, screenAnimationPath, sceneLoadingName, loadingName);
     }
 
-    public static void Load<T>(string sceneName, LoadSceneMode mode, OnSceneLoad<T> onSceneLoaded = null, bool clearAllScreen = true) where T : Component
+    /// <summary>
+    /// Load a scene.
+    /// </summary>
+    /// <typeparam name="T">The type of a (any) component in the scene</typeparam>
+    /// <param name="sceneName">The name of scene</param>
+    /// <param name="mode">The load scene mode. Single or Additive</param>
+    /// <param name="onSceneLoaded">The callback when the scene is loaded. [IMPORTANT] It is called after the first Update, before the first LateUpdate</param>
+    /// <param name="clearAllScreens">Clear all screens when the scene is loaded?</param>
+    public static void Load<T>(string sceneName, LoadSceneMode mode, OnSceneLoad<T> onSceneLoaded = null, bool clearAllScreens = true) where T : Component
     {
-        instance.LoadScene(sceneName, mode, onSceneLoaded, clearAllScreen);
+        instance.LoadScene(sceneName, mode, onSceneLoaded, clearAllScreens);
     }
 
+    /// <summary>
+    /// Add a screen on top of all screens. [IMPORTANT] The code after the 'Add' method will be called after the Awake & OnEnable, before the Start.
+    /// </summary>
+    /// <typeparam name="T">The type of a (any) component in the screen</typeparam>
+    /// <param name="screenName">The name of screen</param>
+    /// <param name="showAnimation">The name of animation clip (which is put in 'screenAnimationPath') is used to animate the screen to show it</param>
+    /// <param name="hideAnimation">The name of animation clip (which is put in 'screenAnimationPath') is used to animate the screen to hide it</param>
+    /// <returns>The component type T in the screen.</returns>
     public static T Add<T>(string screenName, string showAnimation = "ScaleShow", string hideAnimation = "ScaleHide") where T : Component
     {
         return instance.AddScreen<T>(screenName, showAnimation, hideAnimation);
     }
 
+    /// <summary>
+    /// Add a screen to the canvas, on top of all screens. But it's not added to the screen list for managing.
+    /// </summary>
+    /// <param name="screen">The GameObject of screen</param>
     public static void AddToCanvas(GameObject screen)
     {
         instance.AddScreenToCanvas(screen);
     }
 
+    /// <summary>
+    /// Close the screen which is at the top of all screens.
+    /// </summary>
+    /// <param name="onScreenClosed">The callback when the screen is closed. [IMPORTANT] It is called right after the screen is destroyed.</param>
+    /// <param name="hideAnimation">The name of animation clip (which is put in 'screenAnimationPath') is used to animate the screen to hide it</param>
     public static void Close(OnScreenClosed onScreenClosed = null, string hideAnimation = null)
     {
         instance.CloseScreen(onScreenClosed, hideAnimation);
     }
 
+    /// <summary>
+    /// Close a specific screen.
+    /// </summary>
+    /// <param name="screen">The component in screen which is returned by the Add function.</param>
+    /// <param name="onScreenClosed">The callback when the screen is closed. [IMPORTANT] It is called right after the screen is destroyed.</param>
+    /// <param name="hideAnimation">The name of animation clip (which is put in 'screenAnimationPath') is used to animate the screen to hide it</param>
     public static void Close(Component screen, OnScreenClosed onScreenClosed = null, string hideAnimation = null)
     {
         instance.CloseScreen(screen, onScreenClosed, hideAnimation);
     }
 
+    /// <summary>
+    /// Close all screens.
+    /// </summary>
     public static void CloseAll()
     {
         instance.ClearAllScreen();
     }
 
+    /// <summary>
+    /// Show/Hide the loading screen (which has the name 'loadingName') on top of all screens.
+    /// </summary>
+    /// <param name="isShow">True if show, False if hide</param>
     public static void Loading(bool isShow)
     {
         instance.ShowLoading(isShow);
     }
 
+    /// <summary>
+    /// Remove a specific screen. But don't use this. We use this for an internal purpose.
+    /// </summary>
+    /// <param name="screen">The component in screen which is returned by Add function.</param>
     public static void RemoveScreen(Component screen)
     {
         instance.RemoveScreenFromList(screen);
