aa4f5433aade4a4c61cd7fb77caa9d6712b6ebc4
anh-pham <anhpt.csit@gmail.com>
2024-04-06 00:36:26 +0700

    Demo

--- SSv5/Assets/Scenes/Scene1/Scene1.cs
+++ SSv5/Assets/Scenes/Scene1/Scene1.cs
@@ -19,6 +19,16 @@ public class Scene1 : MonoBehaviour
             scene2.cube.localScale = new Vector3(2, 1, 1);
         });
 
+        AddScreen1();
+    }
+
+    public void OnScreen1ButtonTap()
+    {
+        AddScreen1();
+    }
+
+    private void AddScreen1()
+    {
         var screen1 = ScreenManager.Add<Screen1>("Screen1", "RightShow", "RightHide");
         screen1.label.text = "Screen1";
     }
