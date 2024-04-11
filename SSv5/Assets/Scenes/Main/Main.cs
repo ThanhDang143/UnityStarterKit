70ee40b2aa3b37289eada741625571021c9ab589
anh-pham <anhpt.csit@gmail.com>
2024-04-11 13:57:16 +0700

    FPS 60 + Settings

--- SSv5/Assets/Scenes/Main/Main.cs
+++ SSv5/Assets/Scenes/Main/Main.cs
@@ -7,6 +7,8 @@ public class Main : MonoBehaviour
 {
     private void Start()
     {
+        Application.targetFrameRate = 60;
+
         ScreenManager.Set(new Color(0, 0, 0, 0.8f), "Screens", "Animations", "SceneLoading", "Loading");
 
         ScreenManager.Load<Scene1>("Scene1", LoadSceneMode.Single, (scene1) =>
