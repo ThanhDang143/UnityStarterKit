141e842d1e5cf0b37b57aaf8044f97ef5fca92a8
ThanhDV <vanthanh1998@gmail.com>
2024-08-24 22:32:08 +0700

    Update Odin 3317, Update Singleton 002

--- Assets/PluginPackages/Utilities/Singleton/Singleton.cs
+++ Assets/PluginPackages/Utilities/Singleton/Singleton.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
 
-
 public class Singleton<T> where T : class, new()
 {
     private static T _instance;
     private static readonly object _lock = new object();
 
-    private Singleton() { } // Private constructor to prevent object creation from outside the class
-
     public static T Instance
     {
         get
         {
-            lock (_lock) // Lock to ensure thread safety
+            lock (_lock)
             {
                 if (_instance == null)
                 {
+                    Debug.Log($"<color=yellow>{_instance.GetType().Name} instance is null!!! Auto create new instance</color>");
+
                     _instance = new T();
                 }
                 return _instance;
@@ -37,38 +36,12 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
             {
                 if (_instance == null)
                 {
-                    _instance = FindObjectOfType(typeof(T)) as T;
+                    _instance = FindFirstObjectByType(typeof(T)) as T;
 
                     if (_instance == null)
                     {
-                        _instance = new GameObject().AddComponent<T>();
-                        _instance.gameObject.name = _instance.GetType().Name;
-                    }
-                    DontDestroyOnLoad(_instance);
-                }
-                return _instance;
-            }
-        }
-    }
-}
+                        Debug.Log($"<color=yellow>{_instance.GetType().Name} instance is null!!! Auto create new instance</color>");
 
-public class SSSingleton<T> : SSController where T : SSController
-{
-    private static T _instance;
-    private static readonly object _lock = new object();
-
-    public static T Instance
-    {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance == null)
-                {
-                    _instance = FindObjectOfType(typeof(T)) as T;
-
-                    if (_instance == null)
-                    {
                         _instance = new GameObject().AddComponent<T>();
                         _instance.gameObject.name = _instance.GetType().Name;
                     }
