620234b78958a739599f58026a6f319bb13dd15c
ThanhDV <vanthanh1998@gmail.com>
2024-08-24 22:22:23 +0700

    Remove obsolete code

--- Assets/PluginPackages/SSSystem/Scripts/Compatibility/nGUI/SSAutoAddUICamera.cs
+++ Assets/PluginPackages/SSSystem/Scripts/Compatibility/nGUI/SSAutoAddUICamera.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SSAutoAddUICamera : MonoBehaviour 
+public class SSAutoAddUICamera : MonoBehaviour
 {
 	private void Awake()
 	{
-		UnityEngineInternal.APIUpdaterRuntimeServices.AddComponent(gameObject, "Assets/SSSystem/Scripts/Compatibility/nGUI/SSAutoAddUICamera.cs (8,3)", "UICamera");
+		// UnityEngineInternal.APIUpdaterRuntimeServices.AddComponent(gameObject, "Assets/SSSystem/Scripts/Compatibility/nGUI/SSAutoAddUICamera.cs (8,3)", "UICamera");
 	}
 }
