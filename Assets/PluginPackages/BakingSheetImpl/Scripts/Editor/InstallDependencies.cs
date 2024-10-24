0bdffbb10c733e6f049e16b0c59593e80ea777f1
ThanhDV <vanthanh1998@gmail.com>
2024-10-25 02:25:44 +0700

    Fix install dependencies method

--- Assets/PluginPackages/BakingSheetImpl/Scripts/Editor/InstallDependencies.cs
+++ Assets/PluginPackages/BakingSheetImpl/Scripts/Editor/InstallDependencies.cs
@@ -6,69 +6,78 @@ using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
-public class InstallDependencies
+namespace BakingSheetImpl
 {
-    static Dictionary<string, string> packageDependencies = new Dictionary<string, string>
+    [InitializeOnLoad]
+    public class InstallDependencies
+    {
+        static InstallDependencies()
+        {
+            InitializeDependencies();
+        }
+
+        static Dictionary<string, string> packageDependencies = new Dictionary<string, string>
         {
             { "com.cathei.bakingsheet", "https://github.com/cathei/BakingSheet.git?path=UnityProject/Packages/com.cathei.bakingsheet" }
         };
 
-    private static async Task InstallDependenciesPackage(string packageName)
-    {
-        string gitURL = packageDependencies[packageName];
-        AddRequest request = Client.Add(gitURL);
-        float timeOut = 5f;
-
-        while (!request.IsCompleted && timeOut > 0)
+        private static async Task InstallDependenciesPackage(string packageName)
         {
-            timeOut -= Time.deltaTime;
-            await Task.Delay((int)Time.deltaTime * 1000);
-        }
+            string gitURL = packageDependencies[packageName];
+            AddRequest request = Client.Add(gitURL);
+            float timeOut = 5f;
 
-        if (request.Status == StatusCode.Success)
-        {
-            Debug.Log($"<color=green>Install {packageName} success</color>");
-        }
-        else if (request.Status >= StatusCode.Failure)
-        {
-            Debug.Log($"<color=red>Install {packageName} fail with error {request.Error.message}</color>");
-        }
-    }
-
-    [MenuItem("Tools/BakingSheet Impl/Initialize")]
-    public static void InitializeDependencies()
-    {
-        foreach (var package in packageDependencies)
-        {
-            _ = CheckDependenciesPackageInstalled(package.Key);
-        }
-    }
-
-    public static async Task CheckDependenciesPackageInstalled(string packageName)
-    {
-        ListRequest request = Client.List();
-        float timeOut = 5f;
-
-        while (!request.IsCompleted && timeOut > 0)
-        {
-            timeOut -= Time.deltaTime;
-            await Task.Delay((int)Time.deltaTime * 1000);
-        }
-
-        if (request.Status == StatusCode.Success)
-        {
-            if (request.Result.Any(package => package.name == packageName))
+            while (!request.IsCompleted && timeOut > 0)
             {
-                Debug.Log($"<color=green>{packageName} is installed</color>");
+                timeOut -= Time.deltaTime;
+                await Task.Delay((int)Time.deltaTime * 1000);
             }
-            else
+
+            if (request.Status == StatusCode.Success)
             {
-                _ = InstallDependenciesPackage(packageName);
+                Debug.Log($"<color=green>Install {packageName} success</color>");
+            }
+            else if (request.Status >= StatusCode.Failure)
+            {
+                Debug.Log($"<color=red>Install {packageName} fail with error {request.Error.message}</color>");
             }
         }
-        else if (request.Status >= StatusCode.Failure)
+
+        [MenuItem("Tools/BakingSheet Impl/Initialize")]
+        public static void InitializeDependencies()
         {
-            Debug.Log($"<color=red>Check {packageName} fail with error {request.Error.message}</color>");
+            foreach (var package in packageDependencies)
+            {
+                _ = CheckDependenciesPackageInstalled(package.Key);
+            }
+        }
+
+        public static async Task CheckDependenciesPackageInstalled(string packageName)
+        {
+            ListRequest request = Client.List();
+            float timeOut = 5f;
+
+            while (!request.IsCompleted && timeOut > 0)
+            {
+                timeOut -= Time.deltaTime;
+                await Task.Delay((int)Time.deltaTime * 1000);
+            }
+
+            if (request.Status == StatusCode.Success)
+            {
+                if (request.Result.Any(package => package.name == packageName))
+                {
+                    Debug.Log($"<color=green>{packageName} is installed</color>");
+                }
+                else
+                {
+                    _ = InstallDependenciesPackage(packageName);
+                }
+            }
+            else if (request.Status >= StatusCode.Failure)
+            {
+                Debug.Log($"<color=red>Check {packageName} fail with error {request.Error.message}</color>");
+            }
         }
     }
 }
