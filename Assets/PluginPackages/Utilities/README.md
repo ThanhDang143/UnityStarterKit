0f036162bdc1036de05fa68f3dd290c13041b259
ThanhDV <vanthanh1998@gmail.com>
2024-11-06 16:37:44 +0700

    Merge all small Utilities into this branch

---
+++ Assets/PluginPackages/Utilities/README.md
@@ -0,0 +1,16 @@
+# Utilities
+This library contains some utilities for Unity development.
+This library contain:
+- Singleton
+- UIAdaptation
+- RectTransform Extension
+- More utilites.
+## Install
+- GitURL: https://github.com/ThanhDang143/UnityStarterKit.git?path=/Assets/PluginPackages/Utilities#Utilities
+
+## Changelog 
+### v0.0.1
+- Merge `Singleton`, `UIAdaptation`, `RectTransform Extension` into this branch.
+
+## How to use.
+See the README for each utility.
--- Assets/PluginPackages/Utilities/Singleton/Singleton.cs
+++ Assets/PluginPackages/Utilities/Singleton/Singleton.cs
@@ -1,27 +1,28 @@
 using UnityEngine;
 
-
 public class Singleton<T> where T : class, new()
 {
     private static T _instance;
     private static readonly object _lock = new object();
 
-    private Singleton() { } // Private constructor to prevent object creation from outside the class
-
     public static T Instance
     {
         get
         {
-            lock (_lock) // Lock to ensure thread safety
+            lock (_lock)
             {
                 if (_instance == null)
                 {
                     _instance = new T();
+
+                    Debug.Log($"<color=yellow>{_instance.GetType().Name} instance is null!!! Auto create new instance</color>");
                 }
                 return _instance;
             }
         }
     }
+
+    public static bool IsExist => _instance != null;
 }
 
 public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
@@ -37,12 +38,14 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
             {
                 if (_instance == null)
                 {
-                    _instance = FindObjectOfType(typeof(T)) as T;
+                    _instance = FindFirstObjectByType(typeof(T)) as T;
 
                     if (_instance == null)
                     {
                         _instance = new GameObject().AddComponent<T>();
                         _instance.gameObject.name = _instance.GetType().Name;
+
+                        Debug.Log($"<color=yellow>{_instance.GetType().Name} instance is null!!! Auto create new instance</color>");
                     }
                     DontDestroyOnLoad(_instance);
                 }
@@ -50,32 +53,6 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
             }
         }
     }
-}
 
-public class SSSingleton<T> : SSController where T : SSController
-{
-    private static T _instance;
-    private static readonly object _lock = new object();
-
-    public static T Instance
-    {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance == null)
-                {
-                    _instance = FindObjectOfType(typeof(T)) as T;
-
-                    if (_instance == null)
-                    {
-                        _instance = new GameObject().AddComponent<T>();
-                        _instance.gameObject.name = _instance.GetType().Name;
-                    }
-                    DontDestroyOnLoad(_instance);
-                }
-                return _instance;
-            }
-        }
-    }
+    public static bool IsExist => _instance != null;
 }
