9501ea1c9935bd08edbb3325f9c168a2832d5f04
ThanhDV <vanthanh1998@gmail.com>
2025-03-06 04:00:24 +0700

    Update Singleton

--- Assets/CustomPackages/Utilities/Singleton/Singleton.cs
+++ Assets/CustomPackages/Utilities/Singleton/Singleton.cs
@@ -25,6 +25,49 @@ public class Singleton<T> where T : class, new()
     public static bool IsExist => _instance != null;
 }
 
+public class PersistentMonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType(typeof(T)) as T;
+
+                    if (_instance == null)
+                    {
+                        _instance = new GameObject().AddComponent<T>();
+                        _instance.gameObject.name = _instance.GetType().Name;
+
+                        Debug.Log($"<color=yellow>{_instance.GetType().Name} instance is null!!! Auto create new instance</color>");
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public static bool IsExist => _instance != null;
+
+    // Check for case have many instance of T
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+}
+
 public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T _instance;
@@ -55,4 +98,17 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
     }
 
     public static bool IsExist => _instance != null;
+
+    // Check for case have many instance of T
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(_instance);
+            return;
+        }
+
+        Destroy(gameObject);
+    }
 }
